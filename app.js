0b57a63cdd1fd7a4e642777f6509760e4309e81a
Khalil <khalildevo@gmail.com>
2023-08-10 02:51:18 +0500

    asd

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const multer = require("multer");
 const sharp = require("sharp");
 const fs = require("fs");
-const hello;
 const app = express();
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
